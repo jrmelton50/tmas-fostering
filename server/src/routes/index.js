f553ff48507560fb71e35cc74a9a9e7511e5240c
jrmelton50 <jrmelton@crimson.ua.edu>
2018-06-06 21:02:49 -0500

    testing...

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
-import peopleRouter from './people';
-import classesRouter from './classes';
+// import peopleRouter from './people';
+// import classesRouter from './classes';
 import authRouter from './auth';
 import usersRouter from './users';
 import { isLoggedIn, tokenMiddleware } from '../middleware/auth.mw';
@@ -8,15 +8,14 @@ import { isLoggedIn, tokenMiddleware } from '../middleware/auth.mw';
 let router = Router();
 
 router.use('/auth', authRouter);
+
+// router.route('*')
+//     .post(tokenMiddleware, isLoggedIn)
+//     .put(tokenMiddleware, isLoggedIn)
+//     .delete(tokenMiddleware, isLoggedIn);
+
+// router.use('/classes', classesRouter);
+// router.use('/people', peopleRouter);
 router.use('/users', usersRouter);
-
-router.route('*')
-    .post(tokenMiddleware, isLoggedIn)
-    .put(tokenMiddleware, isLoggedIn)
-    .delete(tokenMiddleware, isLoggedIn);
-
-router.use('/classes', classesRouter);
-router.use('/people', peopleRouter);
-// router.use('/users', usersRouter);
 
 export default router;
