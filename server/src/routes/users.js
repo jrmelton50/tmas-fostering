658eea2f1be1601e20406525f6fbbd5816176109
jrmelton50 <jrmelton@crimson.ua.edu>
2018-06-06 20:09:31 -0500

    adding lots of files

---
+++ server/src/routes/users.js
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import { tokenMiddleware, isLoggedIn } from '../middleware/auth.mw';
+
+let router = Router();
+
+router.get('/me', tokenMiddleware, isLoggedIn, (req, res) => {
+    res.json(req.user);
+});
+
+router.post('/', (req, res) => {
+    
+});
+
+export default router;
